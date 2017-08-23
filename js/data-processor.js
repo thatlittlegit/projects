01d5d35981c708a265a4caa7cf779dae1271fd9c
thatlittlegit <wapidstyle@live.ca>
2017-08-23 10:45:05 -0400

    Add language colors

--- js/data-processor.js
+++ js/data-processor.js
@@ -2,10 +2,15 @@ function processApiData(apiData) {
 
 		const elements = _(apiData).map((data) => {
 				setPreview("processing " + (data.fullname || data.full_name));
-				return $(h("div#" + data.name, data.description)).addClass('col-md').addClass('project');
-		}).chunk(4).map((data) => {
-				return $(h('div.row')).html(data);
-		}).value();
+				console.log(data.language);
+				return $(h("div#" + data.name, data.description))
+						.addClass('col-md')
+						.addClass('project')
+				// HACK Assume Java since all my early projects are Java, and those wouldn't have a language.
+						.addClass((data.language === '' ? 'java' : data.language).toLowerCase().replace('/', ''));
+		}).chunk(4).map((data) => (
+				$(h('div.row')).html(data)
+		)).value();
 
 		$('body #main').addClass('container').html(elements);
 }
