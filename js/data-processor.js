86afb9b3be304519153feb9382ce70aa7aa069a9
thatlittlegit <wapidstyle@live.ca>
2017-08-23 12:04:33 -0400

    Add a padding and the repo name to a h2

--- js/data-processor.js
+++ js/data-processor.js
@@ -2,7 +2,7 @@
 function processApiData(apiData) {
 	$('body #main').addClass('container').html(_(apiData).map((data) => {
 		setPreview(`processing ${(data.fullname || data.full_name)}`);
-		return $(h(`div#${data.name}`, data.description))
+		return $(h(`div#${data.name}`, h('h3', data.name), data.description))
 			.addClass('col-md')
 			.addClass('project')
 		// HACK Assume Java since all my early projects are Java, and those wouldn't have a language.
